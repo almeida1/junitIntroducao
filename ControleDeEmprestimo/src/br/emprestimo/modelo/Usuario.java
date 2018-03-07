ee451e43d6f0c72215a033ebf7f775427f471f32
DV6-6170\Hp <engsoftware1@gmail.com>
2018-03-07 15:02:14 -0300

    versao inicial

---
+++ ControleDeEmprestimo/src/br/emprestimo/modelo/Usuario.java
@@ -0,0 +1,22 @@
+package br.emprestimo.modelo;
+
+public class Usuario {
+	private String ra;
+	private String nome;
+
+	public String getRa() {
+		return ra;
+	}
+
+	public void setRa(String ra) {
+		this.ra = ra;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+}
