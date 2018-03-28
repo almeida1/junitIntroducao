8e95019108be7e48a62b349df8faa73c05ebaa18
professor <professor@L1MP.IPIRANGA.FATEC>
2018-03-27 21:54:05 -0300

    Inclusao do DataBuilder

--- ControleDeEmprestimo/teste/br/emprestimo/testeUnitario/UC06CadastrarLivro.java
+++ ControleDeEmprestimo/teste/br/emprestimo/testeUnitario/UC06CadastrarLivro.java
@@ -6,6 +6,7 @@ import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import br.emprestimo.dadosDeTeste.ObtemLivro;
 import br.emprestimo.modelo.Emprestimo;
 import br.emprestimo.modelo.Livro;
 import br.emprestimo.modelo.Usuario;
@@ -15,10 +16,11 @@ public class UC06CadastrarLivro {
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
 		//cenario
-			livro = new Livro();
-			livro.setIsbn("121212");
-			livro.setTitulo("Engenharia de Software");
-			livro.setAutor("Pressman");
+//			livro = new Livro();
+//			livro.setIsbn("121212");
+//			livro.setTitulo("Engenharia de Software");
+//			livro.setAutor("Pressman");
+			livro = ObtemLivro.comDadosValidos();
 	}
 
 	@AfterClass
@@ -30,15 +32,30 @@ public class UC06CadastrarLivro {
 		assertEquals("121212", livro.getIsbn());
 	}
 	
-	
+	@Test
+	public void CT02UC06CadastrarLivro_titulo_valido() {
+		assertEquals("Engenharia de Software", livro.getTitulo());
+	}
+	@Test
+	public void CT03UC06CadastrarLivro_autor_valido() {
+		assertEquals("Pressman", livro.getAutor());
+	}
 	@Test(expected=IllegalArgumentException.class)
-	public void CT02UC06CadastrarLivro_isbn_invalido_branco() {
+	public void CT04UC06CadastrarLivro_isbn_invalido_branco() {
 		livro.setIsbn("");
 	}
+	
 	@Test(expected=RuntimeException.class)
-	public void CT03UC06CadastrarLivro_isbn_invalido_nulo() {
+	public void CT05UC06CadastrarLivro_isbn_invalido_nulo() {
 		livro.setIsbn(null);
 	}
-	
+	@Test(expected=IllegalArgumentException.class)
+	public void CT06UC06CadastrarLivro_isbn_invalido_branco() {
+		ObtemLivro.comISBNInvalido_branco();
+	}
+	@Test(expected=IllegalArgumentException.class)
+	public void CT07UC06CadastrarLivro_isbn_nulo() {
+		ObtemLivro.comISBNInvalido_nulo();
+	}
 
 }
