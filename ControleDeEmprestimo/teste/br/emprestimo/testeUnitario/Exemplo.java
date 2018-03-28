c593751db56482645f76ea7326c1cce2870f2219
professor <professor@L1MP.IPIRANGA.FATEC>
2018-03-27 22:06:43 -0300

    Exemplo de analise de emprestimos

---
+++ ControleDeEmprestimo/teste/br/emprestimo/testeUnitario/Exemplo.java
@@ -0,0 +1,25 @@
+package br.emprestimo.testeUnitario;
+
+public class Exemplo {
+	
+//	public boolean verificaVigencia(Convenio novoConvenio) {
+//		List<Convenio> listaDeConvenios = new ArrayList<Convenio>();
+//		listaDeConvenios = consultaConvenio(novoConvenio.getCNPJ());
+//		if (listaDeConvenios.size() > 0) {// existe convenio para este cnpj
+//			for (Convenio convenio : listaDeConvenios) {
+//				Interval interval = new Interval(convenio.getDataInicio(), convenio.getDataTermino());
+//				logger.info("verificaVigencia entre " + convenio.getDataInicio() + " - " + convenio.getDataTermino());
+//				if (interval.contains(novoConvenio.getDataTermino())) {
+//					logger.info("verificaVigencia achou conflito de datas para data termino ");
+//					return true;// achou conflito no periodo de vigencia
+//				}
+//				if (interval.contains(novoConvenio.getDataInicio())) {
+//					logger.info("verificaVigencia achou conflito de datas para data inicio ");
+//					return true;// achou conflito no periodo de vigencia
+//				}
+//			}
+//		}
+//		return false; // nao achou conflito no periodo de vingencia
+//	}
+
+}
